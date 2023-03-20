76d4219e7ec1317f2fa0f2e1b00e261918e31e8f
Anton-Osip <osipchika@gmail.com>
2023-03-20 18:05:58 +0300

    6.3 Введение в unit тесты и jest

---
+++ src/example.test.ts
@@ -0,0 +1,10 @@
+describe('add operator', () => {
+	test('shoul return 2 when add one and one', () => {
+		expect(1 + 1).toBe(2)
+	})
+	describe('first', () => {
+		test('shoul return 2 when add one and one', () => {
+			expect(1 + 1).toBe(2)
+		})
+	})
+})
