1f605efa0f8aaece1d8c2b14a28d55127d9e8ac2
Anton-Osip <osipchika@gmail.com>
2023-03-21 17:57:23 +0300

    8.4Обновляем данные через контекст

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -7,20 +7,22 @@ import { Content } from './Content'
 import { CardsList } from './CardsList'
 import { useToken } from '../hooks/useToken'
 import { tokenContext } from '../context/tokenContext'
+import { UserContextProvider } from '../context/userContext'
 
 function AppComponent() {
 	const [token] = useToken()
-	const { Provider } = tokenContext
 
 	return (
-		<Provider value={token}>
-			<Layout>
-				<Header />
-				<Content>
-					<CardsList />
-				</Content>
-			</Layout>
-		</Provider>
+		<tokenContext.Provider value={token}>
+			<UserContextProvider>
+				<Layout>
+					<Header />
+					<Content>
+						<CardsList />
+					</Content>
+				</Layout>
+			</UserContextProvider>
+		</tokenContext.Provider>
 	)
 }
 
